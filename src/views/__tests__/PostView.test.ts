6a2012f07b096b570bce0343f2d671768eb51765
sei <29907321+seimmuc@users.noreply.github.com>
2022-11-01 20:46:19 -0700

    changed api mocking in tests and the test data

--- src/views/__tests__/PostView.test.ts
+++ src/views/__tests__/PostView.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { describe, it, expect, vi, beforeEach, afterAll, beforeAll } from 'vitest'
 
 import { flushPromises, mount } from '@vue/test-utils'
 import { routes } from '../../router/index';
@@ -6,23 +6,22 @@ import PostView from '../PostView.vue';
 import { createWebHistory } from 'vue-router';
 import { createRouterMock, injectRouterMock, type RouterMock, getRouter } from 'vue-router-mock'
 import createFetchMock from 'vitest-fetch-mock';
-import postData from './PostView.data.json';
+import { fetchHome, fetchPool, fetchPost } from '@/api';// '../../api';
+import { startMocking, stopMocking } from './TestDataMocker';
 
 
-const mountGlobal = { stubs: ['FontAwesomeIcon'] };
-const fetchMock = createFetchMock(vi);
-fetchMock.enableMocks();
-
-// mock posts API /api/post/<id>
-const postUrlRegex = /\/api\/post\/(\d+)\/?(?:\?.*)?/
-fetchMock.doMock((req) => {
-  const match = req.url.match(postUrlRegex)
-  if (!match || !match[1] || !(match[1] in postData))
-    return { status: 404, body: '{}' };
-  return { status: 200, body: JSON.stringify(postData[<keyof typeof postData> match[1]]) };
-});
+// vi.mock("@/api", async () => {
+//   const api: object = await vi.importActual('@/api');
+//   return {
+//     ...api,
+//     getPost: async (): Promise<IPost> => {
+//       return {id: 1, description: 'string', file_url: 'strings'}
+//     }
+//   }
+// });
 
 describe('PostView', () => {
+  const mountGlobal = { stubs: ['FontAwesomeIcon'] };
   const router: RouterMock = createRouterMock({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes,
@@ -31,9 +30,34 @@ describe('PostView', () => {
       reset: spy => spy.mockReset(),
     },
   });
+  const fetchMock = createFetchMock(vi);
+
+  beforeAll(() => {
+    startMocking(fetchMock);
+  });
+  afterAll(() => {
+    stopMocking(fetchMock);
+  });
+  
   beforeEach(() => {
     injectRouterMock(router);
-  })
+  });
+  
+  it('api mocking', async () => {
+    const home = await fetchHome();
+    expect(home?.pools.find(p => p.name == 'all')?.size).toEqual(5);
+    expect(home?.pools.find(p => p.name == 'onepostpool')?.posts?.map(p => p.id)).toEqual([4]);
+    const poolAll = await fetchPool('all');
+    expect(poolAll?.posts?.[2].file_url).toEqual('/images/nameless_image.gif');
+    const postNameless = await fetchPost(3);
+    expect(postNameless?.name).toBeUndefined();
+    expect(postNameless?.pools?.find(p => p.name == 'testpool')).toBeDefined();
+    expect((await fetchPool('testpool'))?.posts?.map(p => p.id)).toContain(postNameless?.id);
+    expect((await fetchPost(6))?.pools).toHaveLength(0);
+    const nlNextId = postNameless?.pools?.find(p => p.name == 'testpool')?.nextid;
+    expect(nlNextId).toEqual(5);
+    expect((await fetchPost(<number>nlNextId))).toHaveProperty('description', 'last post');
+  });
 
   it('first post', async () => {
     // make sure router mocking worked
@@ -44,8 +68,6 @@ describe('PostView', () => {
     router.push({name: 'post', params: {id: 1}});
     router.setParams({id: 1});
     await flushPromises();
-    // await wrapper.vm.$nextTick();
-    // wrapper.vm.$router
     
     expect(wrapper.html()).toMatchSnapshot();
   });
