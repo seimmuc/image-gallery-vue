9833bce1d005796870e6c27ac8db99173addc691
sei <29907321+seimmuc@users.noreply.github.com>
2022-11-06 01:19:27 -0700

    switched main style files to SASS (SCSS)

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 library.add(faChevronLeft, faChevronRight)
 
